8234bf18cee9616fbbbe007e7bb93e27d808cf12
kali <fs@gmail.com>
2021-04-19 11:20:14 +0800

    修改MainController 所存在的bug

--- src/main/java/com/bankapi/bankapi/controller/MainController.java
+++ src/main/java/com/bankapi/bankapi/controller/MainController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
@@ -26,9 +27,9 @@ public class MainController {
     @Autowired
     UserServiceIml userServiceIml;
 
+    @ResponseBody
     @RequestMapping(value = "getAllUsers",method = RequestMethod.GET)
     public List<User> getAllUsers(){
-
         return userServiceIml.findAllUsers();
     }
 }
--- src/main/java/com/bankapi/bankapi/sevice/iml/UserServiceIml.java
+++ src/main/java/com/bankapi/bankapi/sevice/iml/UserServiceIml.java
@@ -4,6 +4,7 @@ import com.bankapi.bankapi.dao.UserDao;
 import com.bankapi.bankapi.model.User;
 import com.bankapi.bankapi.sevice.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
@@ -16,6 +17,7 @@ import java.util.List;
  * @createDate: 2021-04-17  21:02
  * @description: UserService 实现类
  **/
+@Service
 public class UserServiceIml implements UserService {
     @Autowired
     UserDao userDao;
